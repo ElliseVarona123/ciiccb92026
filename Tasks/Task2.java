69837dfcfc06336d8d2105207cd8a3defeb5d6ed
ElliseVarona123 <zairaellisevarona@gmail.com>
2026-07-25 13:46:57 +0800

    Fix string concatenation in Task2.java

---
+++ Tasks/Task2.java
@@ -0,0 +1,14 @@
+
+public class Task2 {
+    public static void main(String[] args) {
+        char letter = 'H';
+        byte b = 31;
+        short s = 1;
+        int i = 0;
+        float f = 2.0f;
+        boolean flag = true;
+
+        String output = letter + 1"" + b + s + i + " wOrld " + f + " " + flag;
+        System.out.println(output);
+    }
+}
